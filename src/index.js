7a7ea3d2588c4bc15bdb182577b73b92dc8583c9
Michael Chen <michaelchengaming@gmail.com>
2020-01-30 18:51:53 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import Navbar from "./Navbar";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  NavLink
+} from "react-router-dom";
 
 ReactDOM.render(
-  <Home />,
-  document.getElementById('root')
+  <Router>
+    <Switch>
+      <divt>
+        <Navbar />
+        <Route path="/" exact component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/login" component={Login} />
+      </divt>
+    </Switch>
+  </Router>,
+  document.getElementById("root")
 );
